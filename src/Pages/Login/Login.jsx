5f590ba283ebd5b0a787508d7a7133a103a11111
Ashiqur-Anik <ashiqur1801@gmail.com>
2023-10-03 22:36:53 +0600

    added leftSideNav

---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../Shared/Navbar/Navbar";
+
+const Login = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <h1>this is Login</h1>
+        </div>
+    );
+};
+
+export default Login;
